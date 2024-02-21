c8764270c538257297ee60debc89ca90f82562ef
Tieng <vinhtieng123@gmail.com>
2024-02-21 13:10:30 +0700

    Add remote module and expose new functions

--- src/renderer.js
+++ src/renderer.js
@@ -1,26 +1,34 @@
 navigator.mediaDevices.getDisplayMedia = async () => {
-    try {
-      const selectedSource = await globalThis.myCustomGetDisplayMedia();
-  
-      // create MediaStream
-      const stream = await navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: {
-          mandatory: {
-            chromeMediaSource: "desktop",
-            chromeMediaSourceId: selectedSource.id,
-            minWidth: 1280,
-            maxWidth: 1280,
-            minHeight: 720,
-            maxHeight: 720,
-          },
+  try {
+    const selectedSource = await globalThis.getDisplayMedia();
+
+    // create MediaStream
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+        mandatory: {
+          chromeMediaSource: "desktop",
+          chromeMediaSourceId: selectedSource.id,
+          minWidth: 1280,
+          maxWidth: 1280,
+          minHeight: 720,
+          maxHeight: 720,
         },
-      });
-  
-      return stream;
-    } catch (error) {
-      console.error("Error accessing screen:", error);
-      throw error; // Rethrow the error for further handling, if necessary
-    }
-  };
-  
+      },
+    });
+
+    return stream;
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+navigator.mediaDevices.getAllSources = async () => {
+  try {
+    const sources = await globalThis.getAllAvailableSources();
+    return sources;
+  } catch (error) {
+    throw error; 
+  }
+};
