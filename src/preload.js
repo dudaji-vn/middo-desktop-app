2fad97ab00fcfd936fc004bb493f9f9a354463a8
Tieng <vinh.tieng@dudaji.vn>
2024-02-26 13:21:37 +0700

    Add CSS override and style.css file

--- src/preload.js
+++ src/preload.js
@@ -1,1 +1,7 @@
 require("./overrides/share-screen/index");
+window.addEventListener("DOMContentLoaded", () => {
+  // override css
+  const style = document.createElement("style");
+  style.textContent = readFileSync(join(__dirname, "style.css"), "utf8");
+  document.head.appendChild(style);
+});
